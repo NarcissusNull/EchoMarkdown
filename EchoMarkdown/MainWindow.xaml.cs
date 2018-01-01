3625dac66bca5078ef65767514981e6dc685a2fd
NarcissusNull <1169714441@qq.com>
2018-01-02 00:26:36 +0800

    完成最基本功能

--- EchoMarkdown/MainWindow.xaml.cs
+++ EchoMarkdown/MainWindow.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using EchoMarkdown.ViewModels;
 using System.Windows;
 using System.Configuration;
-using MarkdownSharp;
 using EchoMarkdown.Models;
+using Microsoft.Win32;
+using System.IO;
+using EchoMarkdown.Views;
 
 namespace EchoMarkdown
 {
@@ -13,6 +15,7 @@ namespace EchoMarkdown
     {
         private MainWindowViewModel mainWindowViewModel = new MainWindowViewModel();
         private TempHtml tempHtml                       = new TempHtml();
+        private string choose;
 
         public MainWindow()
         {
@@ -31,44 +34,97 @@ namespace EchoMarkdown
 
         private void MenuNew_Click(object sender, RoutedEventArgs e)
         {
-            if(mainWindowViewModel.IsSaved)
+            if(!mainWindowViewModel.IsSaved)
             {
-                mainWindowViewModel.CurrentFileName = "new.md";
-                mainWindowViewModel.IsSaved = false;
-                textBox.Clear();
+                NotSaved notSaved = new NotSaved(mainWindowViewModel.CurrentFileName);
+                notSaved.Owner = this;
+                notSaved.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+                notSaved.ShowDialog();
+                choose = notSaved.Choose;
+                if (choose.Equals("cancel"))
+                {
+                    return;
+                }
+                if (choose.Equals("yes"))
+                {
+                    StreamWriter stream = new StreamWriter(mainWindowViewModel.CurrentFileName);
+                    stream.Write(textBox.Text);
+                    stream.Flush();
+                    stream.Close();
+                }
             }
-            else
-            {
-                //TODO[未保存界面]
-            }
+            mainWindowViewModel.CurrentFileName = "new.md";
+            textBox.Clear();
+            mainWindowViewModel.IsSaved = true;
         }
 
         private void MenuOpen_Click(object sender, RoutedEventArgs e)
         {
-
+            if(!mainWindowViewModel.IsSaved)
+            {
+                NotSaved notSaved = new NotSaved(mainWindowViewModel.CurrentFileName);
+                notSaved.Owner = this;
+                notSaved.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+                notSaved.ShowDialog();
+                choose = notSaved.Choose;
+                if (choose.Equals("cancel"))
+                {
+                    return;
+                }
+                if (choose.Equals("yes"))
+                {
+                    StreamWriter stream = new StreamWriter(mainWindowViewModel.CurrentFileName);
+                    stream.Write(textBox.Text);
+                    stream.Flush();
+                    stream.Close();
+                }
+            }
+            OpenFileDialog dialog = new OpenFileDialog
+            {
+                Multiselect = false,
+                Title = "选择文件",
+                Filter = "Markdown文件(*.md)|*.md"
+            };
+            if (dialog.ShowDialog() == true)
+            {
+                mainWindowViewModel.CurrentFileName = dialog.FileName;
+                textBox.Text = File.ReadAllText(mainWindowViewModel.CurrentFileName);
+                mainWindowViewModel.IsSaved = true;
+            }
         }
 
         private void MenuSave_Click(object sender, RoutedEventArgs e)
         {
-
+            if(!mainWindowViewModel.IsSaved)
+            {
+                StreamWriter stream = new StreamWriter(mainWindowViewModel.CurrentFileName);
+                stream.Write(textBox.Text);
+                stream.Flush();
+                stream.Close();
+                mainWindowViewModel.IsSaved = true;
+            }
         }
 
         private void MenuSaveAs_Click(object sender, RoutedEventArgs e)
         {
+            SaveFileDialog dialog = new SaveFileDialog
+            {
+                Title = "保存文件",
+                Filter = "Markdown文件(*.md)|*.md",
+                FileName = "Markdown.md"
+             };
 
+             if (dialog.ShowDialog() == true)
+             {
+                 StreamWriter stream = new StreamWriter(dialog.FileName);
+                 stream.Write(textBox.Text);
+                 stream.Flush();
+                 stream.Close();
+                 mainWindowViewModel.IsSaved = true;
+             }
         }
 
         private void MenuSearch_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void MenuUndo_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void MenuRedo_Click(object sender, RoutedEventArgs e)
         {
 
         }
@@ -105,7 +161,7 @@ namespace EchoMarkdown
 
         private void MenuGetcode_Click(object sender, RoutedEventArgs e)
         {
-
+            System.Diagnostics.Process.Start("https://github.com/NarcissusNull/EchoMarkdown");
         }
 
         private void MenuAbout_Click(object sender, RoutedEventArgs e)
@@ -115,6 +171,7 @@ namespace EchoMarkdown
 
         private void TextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
         {
+            mainWindowViewModel.IsSaved = false;
             this.web.NavigateToString(tempHtml.Header + tempHtml.Markdown.Transform(textBox.Text));
         }
     }
---
+++ EchoMarkdown/Views/NotSaved.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+
+namespace EchoMarkdown.Views
+{
+    /// <summary>
+    /// NotSaved.xaml 的交互逻辑
+    /// </summary>
+    public partial class NotSaved : Window
+    {
+        private string choose;
+
+        public string Choose { get => choose; set => choose = value; }
+
+        public NotSaved(string currentFileName)
+        {
+            InitializeComponent();
+            textBox.Text = "是否保存当前文档到:" + currentFileName;
+        }
+
+        private void MenuYes_Click(object sender, RoutedEventArgs e)
+        {
+            Choose = "yes";
+            this.Hide();
+        }
+
+        private void MenuNo_Click(object sender, RoutedEventArgs e)
+        {
+            Choose = "no";
+            this.Hide();
+        }
+
+        private void MenuCancel_Click(object sender, RoutedEventArgs e)
+        {
+            Choose = "cancel";
+            this.Hide();
+        }
+    }
+}
